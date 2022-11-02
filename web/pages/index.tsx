9441b8cfef13b4b01c220b9b72da4d6dc1d925d7
tiagobarros01 <tbarros1337@hotmail.com>
2022-11-01 21:28:28 -0300

    [REFACTOR]: Adds lean code

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,27 +1,23 @@
-import type { GetServerSideProps } from "next"
-import { FunctionComponent } from "react"
+import type { GetServerSideProps } from 'next';
+import type { FunctionComponent } from 'react';
 
 type HomeProps = {
-  count: number,
-}
+  count: number;
+};
 
 const Home: FunctionComponent<HomeProps> = (props) => {
-  return (
-    <h1>{props.count}</h1>
-  )
-}
+  return <h1>{props.count}</h1>;
+};
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const response = await fetch('http://localhost:3333/pools/count');
-  const data = await response.json()
-
-  console.log(data)
+  const data = await response.json();
 
   return {
     props: {
       count: data.count,
-    }
-  }
-}
+    },
+  };
+};
 
 export default Home;
