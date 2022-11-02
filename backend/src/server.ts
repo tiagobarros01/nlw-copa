f136709c6f71b0a08d437bc8c3b11df7c72cd4c1
tiagobarros01 <tbarros1337@hotmail.com>
2022-11-02 11:31:44 -0300

    [FEAT]: Creates a create Pool route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import cors from '@fastify/cors';
 import Fastify from 'fastify';
+import { z } from 'zod';
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -13,12 +15,32 @@ async function bootstrap() {
 
   await fastify.register(cors, {
     origin: true,
-  })
+  });
 
   fastify.get('/pools/count', async () => {
     const pools = await prisma.pool.count();
 
     return { count: pools };
+  });
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generateUniqueId = new ShortUniqueId({ length: 6 });
+    const uniqueCode = String(generateUniqueId()).toLocaleUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code: uniqueCode,
+      },
+    });
+
+    return reply.status(201).send({ uniqueCode });
   });
 
   await fastify.listen({ port: 3333, host: '0.0.0.0' });
