817d51ad2c0903379f8b36c97b095e3ba53b9840
tiagobarros01 <tbarros1337@hotmail.com>
2022-11-01 21:04:59 -0300

    [FEAT]: Takes pools counts and adds cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors';
 import Fastify from 'fastify';
+
+const prisma = new PrismaClient({
+  log: ['query'],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get('/pools/count', () => ({count: 1232131}))
+  await fastify.register(cors, {
+    origin: true,
+  })
 
-  await fastify.listen({ port: 3333 });
+  fastify.get('/pools/count', async () => {
+    const pools = await prisma.pool.count();
+
+    return { count: pools };
+  });
+
+  await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 bootstrap();
